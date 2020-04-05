c540d7a2c232e2bf5708fd6330a996a5c8bc544e
Pepe González <pepeglezabad@gmail.com>
2020-04-05 14:42:31 +0200

    Update geoip.cpp

--- web/geoip.cpp
+++ web/geoip.cpp
@@ -1,6 +1,5 @@
+// cURL to this site: http://www.geoplugin.net/json.gp?ip=
 #include <iostream>
 #include <curl/curl.h>
 
-int main(int argc, char* argv[]) {
-
-}
+int main(int argc, char* argv[]) {}
