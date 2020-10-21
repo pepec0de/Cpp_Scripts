1bd6fd5d5d046f73449a8026757b2ea7528f6424
Pepe González <pepeglezabad@gmail.com>
2020-10-21 19:14:10 +0200

    Add files via upload

---
+++ eqsolver/include/equtils.hpp
@@ -0,0 +1,20 @@
+#ifndef EQUTILS_H
+#define EQUTILS_H
+
+class equtils
+{
+    public:
+        bool hasVar(string element);
+        bool hasVarWpow(String element);
+        float getVarPow(string element);
+        float getElementVar(string element);
+        /*
+        equtils();
+        virtual ~equtils();
+        */
+    protected:
+
+    private:
+};
+
+#endif // EQUTILS_H
