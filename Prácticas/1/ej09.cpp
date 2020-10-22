848e99141d996baf0e3662651ce15731f5242edf
Pepe González <pepeglezabad@gmail.com>
2020-10-22 12:26:41 +0200

    Update ej09.cpp

--- Prácticas/1/ej09.cpp
+++ Prácticas/1/ej09.cpp
@@ -10,8 +10,8 @@ int main() {
 	cout << "Inserte el valor del radio: ";
 	cin >> radio;
 	cout << endl;
-	cout << "Longitud: " << 2*PI*radio << endl;
-	cout << "Superficie: " << PI*pow(radio, 2) << endl;
-	cout << "Esfera: " << 4*PI*pow(radio, 3)/3 << endl;
+	cout << "Longitud de la circunferencia: " << 2*PI*radio << " u" << endl;
+	cout << "Superficie del circulo: " << PI*pow(radio, 2) << " u" << endl;
+	cout << "Volumen de la esfera: " << 4*PI*pow(radio, 3)/3 << " u" << endl;
 	return 0;
 }
