4d897f4af8308a0a0269d0b4e00fdbda1001193b
Pepe González <pepeglezabad@gmail.com>
2020-10-24 18:06:09 +0200

    Create ej17.cpp

---
+++ Universidad/Primero/Prácticas/Tema 2/ej17.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class empleado {
+	int horasNormales, horasExtras;
+	
+	public:
+		// Funcion para pedira por teclado las horas normales y las horas extras trabajadas
+		void cargar();
+		// Funcion para obtener el sueldo que cobrará después de la retención
+		float nomina();
+};
+
+int main() {
+	
+}
