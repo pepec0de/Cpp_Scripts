7d68252a60bda8b0eebcf324c94c903712ff38dd
Pepe González <pepeglezabad@gmail.com>
2020-10-24 11:41:56 +0200

    Update ej12.cpp

--- Universidad/Primero/Prácticas/Tema 2/ej12.cpp
+++ Universidad/Primero/Prácticas/Tema 2/ej12.cpp
@@ -18,13 +18,16 @@ int main() {
 	
 	// Comprobamos que no nos salga una raiz imaginaria
 	if ( pow(b, 2) -4*a*c < 0 ) {
-		cout << "ERROR: No se puede resolver la raiz cuadrada de un entero negativo!" << endl;
+		cout << "ERROR (" << b << "^2 -4*" << a << "*" << c << " = " << pow(b, 2) -4*a*c << 
+			"): No se puede resolver la raiz cuadrada de un entero negativo!" << endl;
 		return 0;
 	}
-	// Hacemos el cast para que no se produzca truncamiento
-	float resultado1 = (float) ( -1*b + sqrt(pow(b, 2) -4*a*c)/2*a )
-	float resultado2 = (float) ( -1*b - sqrt(pow(b, 2) -4*a*c)/2*a )
+	
+	// Aplicamos las formulas
+	float resultado1 = (-b + sqrt(pow(b, 2) -4*a*c)) / (2*a);
+	float resultado2 = (-b - sqrt(pow(b, 2) -4*a*c)) / (2*a);
 	
 	cout << "X1 = " << resultado1 << endl;
 	cout << "X2 = " << resultado2 << endl;
+	return 0;
 }
