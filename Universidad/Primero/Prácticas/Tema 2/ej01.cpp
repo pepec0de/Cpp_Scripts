4a593edc1c8b46f17117b8bf17e9ba01577f1a5d
Pepe González <pepeglezabad@gmail.com>
2020-10-24 11:07:44 +0200

    Update ej01.cpp

--- Universidad/Primero/Prácticas/Tema 2/ej01.cpp
+++ Universidad/Primero/Prácticas/Tema 2/ej01.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 	int entero;
 	
-	cout << "Escriba un numero entero: " << endl;
+	cout << "Escriba un numero entero: ";
 	cin >> entero;
 	cout << endl << "Su numero entero incrementado en 5: " << entero+5 << endl;
 }
