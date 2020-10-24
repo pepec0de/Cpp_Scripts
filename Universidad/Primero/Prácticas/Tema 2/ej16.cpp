c630e401c69cc94237cc525cc8e26a51c0bcd334
Pepe González <pepeglezabad@gmail.com>
2020-10-24 17:56:55 +0200

    Update and rename ej18.cpp to ej16.cpp

--- Universidad/Primero/Prácticas/Tema 2/ej16.cpp
+++ Universidad/Primero/Prácticas/Tema 2/ej16.cpp
@@ -3,8 +3,10 @@
 using namespace std;
 
 int main() {
+	// Obviamente el resultado es el mismo
 	float longitud_en_metros = 15000;
 	float Precio_total_en_euros1 = 5000*(longitud_en_metros/5280.0);
 	float Precio_total_en_euros2 = 5000*(longitud_en_metros/5280);
 	cout << Precio_total_en_euros1 << endl << Precio_total_en_euros2 << endl;
+	return 0;
 }
