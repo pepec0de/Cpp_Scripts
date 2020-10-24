0d4d2e848f62e179d639b3817439d23a51204c0b
Pepe González <pepeglezabad@gmail.com>
2020-10-24 12:22:32 +0200

    Update ej13.cpp

--- Universidad/Primero/Prácticas/Tema 2/ej13.cpp
+++ Universidad/Primero/Prácticas/Tema 2/ej13.cpp
@@ -6,15 +6,16 @@ int main() {
 	// Como se deben recoger unos cuantos valores he decidido 
 	// crear una matriz para no repetir codigo.
 	float notas[5];
+	// Tomamos el tamaño de la matriz:
+	unsigned int tNotas = sizeof(notas)/sizeof(notas[0]);
 	float sumatorio;
-	
-	for (unsigned int i = 0; i < sizeof(notas); i++) {
-		cout << "Introduzca valor de nota " << i << ": ";
+
+	for (unsigned int i = 0; i < tNotas; i++) {
+		cout << "Introduzca valor de nota " << i+1 << ": ";
 		cin >> notas[i];
 		sumatorio += notas[i];
 	}
-	
-	cout << "Su media es: " << sumatorio/sizeof(notas) << endl;
+
+	cout << "Su nota media es: " << sumatorio/tNotas << endl;
 	return 0;
-	
 }
