33218354f87682c12b087f3e3950d4a0632083e4
Pepe González <pepeglezabad@gmail.com>
2020-10-24 18:09:47 +0200

    Update ej16.cpp

--- Universidad/Primero/Prácticas/Tema 2/ej16.cpp
+++ Universidad/Primero/Prácticas/Tema 2/ej16.cpp
@@ -3,10 +3,18 @@
 using namespace std;
 
 int main() {
-	// Obviamente el resultado es el mismo
-	float longitud_en_metros = 15000;
-	float Precio_total_en_euros1 = 5000*(longitud_en_metros/5280.0);
-	float Precio_total_en_euros2 = 5000*(longitud_en_metros/5280);
-	cout << Precio_total_en_euros1 << endl << Precio_total_en_euros2 << endl;
+	int longitud_en_metros = 15000;
+	float Precio_total_en_euros;
+	
+	// Con este valor se indica que la division será entre un entero y punto flotante 
+	// por lo que no se realizara truncamiento de los decimales y se imprimira el valor con decimales
+	Precio_total_en_euros = 5000*(longitud_en_metros/5280.0);
+	cout << Precio_total_en_euros << endl;
+	
+	// En este otro valor se declara que la division sea entre dos valores enteros por lo que
+	// no se asignaran numeros decimales en el valor de la variable final
+	Precio_total_en_euros = 5000*(longitud_en_metros/5280);
+	cout << Precio_total_en_euros << endl;
+	
 	return 0;
 }
