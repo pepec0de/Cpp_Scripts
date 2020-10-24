c2a370c3701b7e06c162fe743212f86a44bb9ea4
Pepe González <pepeglezabad@gmail.com>
2020-10-24 18:59:48 +0200

    Update ej19.cpp

--- Universidad/Primero/Prácticas/Tema 2/ej19.cpp
+++ Universidad/Primero/Prácticas/Tema 2/ej19.cpp
@@ -12,13 +12,13 @@ class Movimiento {
 };
  
 void Movimiento::pedirDatos() {
-	cout << "Introduzca la velocidad inicial: ";
+	cout << "Introduzca la velocidad inicial (m/s): ";
 	cin >> velO;
-	cout << "Introduzca el espacio inicial: ";
+	cout << "Introduzca el espacio inicial (m): ";
 	cin >> spcO;
-	cout << "Introduzca la aceleracion: ";
+	cout << "Introduzca la aceleracion (m/s^2): ";
 	cin >> a;
-	cout << "Introduzca el tiempo: ";
+	cout << "Introduzca el tiempo (seg): ";
 	cin >> t;
 }
 
@@ -33,7 +33,9 @@ float Movimiento::espacioFinal() {
 int main() {
 	Movimiento m;
 	m.pedirDatos();
+	cout << endl;
 	cout << "Aplicamos las formulas:\nVf = Vo + a * t\nS = So + Vo * t * 0.5 * a * t^2\n";
+	cout << endl << "Resultados: " << endl;
 	cout << "La velocidad final es de: " << m.velocidadFinal() << endl;
 	cout << "El espacio final es de : " << m.espacioFinal() << endl;
 	return 0;
