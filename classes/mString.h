726c2feef2ce85478aa4030db2364e3b8bcae816
Pepe González <pepeglezabad@gmail.com>
2021-02-05 17:38:43 +0100

    Custom String header

--- classes/mString.h
+++ classes/mString.h
@@ -12,8 +12,23 @@ class String : string {
 
     public:
         String(string _str) { str = _str; }
-        vector<string> split(string regex);
-        /*
+
+        vector<string> split(string regex) {
+            vector<string> vct;
+            unsigned int relstart = 0;
+            for (unsigned int i = 0; i < str.size(); i++) {
+                // Substring nd add to the vector
+                if (str[i] == regex[0]) {
+                    if (getSubstring(i, regex.size()+i) == regex) {
+                        vct.push_back(getSubstring(relstart, i));
+                        relstart = i + regex.size();
+                    }
+                }
+            }
+            vct.push_back(getSubstring(relstart, str.size()));
+            return vct;
+        }
+        
         vector<string> split(char regex) {
             vector<string> vct;
             unsigned int relstart = 0;
@@ -27,13 +42,42 @@ class String : string {
             vct.push_back(getSubstring(relstart, str.size()));
             return vct;
         }
-        */
+        
         string getSubstring(unsigned int idxStart, unsigned int end) {
             string result = "";
-            for (unsigned int i = start; i < end; i++) {
+            for (unsigned int i = idxStart; i < end; i++) {
                 result += str[i];
+            }
+            return result;
+        }
+
+        string replaceAll(string regex, string replacement) {
+            string result = str;
+            for (unsigned int i = 0; i < result.size(); i++) {
+                // We check if the first char of regex matches with curr char
+                int relstart = 0;
+                if (str[i] == regex[0]) {
+                    if (regex == getSubstring(i, regex.size()+i)) {
+                        result = getSubstring(relstart, i) + replacement
+                            + getSubstring(regex.size()+i, result.size());
+                        relstart = i+replacement.size();
+                    }
+                }
+            }
+            return result;
+        }
+
+        string replaceAll(char regex, char replacement) {
+            string result = str;
+            int relstart = 0;
+            for (unsigned int i = 0; i < result.size(); i++) {
+                if (str[i] == regex) {
+                    result = getSubstring(relstart, i) + replacement
+                        + getSubstring(i+1, result.size());
+                }
             }
             return result;
         }
         
 };
+#endif // MSTRING_H
